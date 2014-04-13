4f30285ec7a93bfdc36bd78c73f4ff19da076795
cyantree <mail@cyantree.com>
2014-04-13 14:46:16 +0200

    * Initial commit

---
+++ source/Actions/CheckAuthorizationAction.php
@@ -0,0 +1,35 @@
+<?php
+namespace Grout\Cyantree\BasicHttpAuthorizationModule\Actions;
+
+use Cyantree\Grout\App\Task;
+
+class CheckAuthorizationAction
+{
+    /** @var Task */
+    public $task;
+    public $module;
+
+    public $username;
+    public $password;
+
+    public function execute()
+    {
+        if($this->username === null || $this->password === null){
+            return false;
+        }else{
+            if($this->task->request->server->has('PHP_AUTH_PW')){
+                return $this->username === $this->task->request->server->get('PHP_AUTH_USER') &&
+                $this->password === $this->task->request->server->get('PHP_AUTH_PW');
+            }else{
+                $authorization = $this->task->request->config->get('Grout_Authorization');
+
+                if (preg_match('@^Basic (.+)$@', $authorization, $authorization)) {
+                    $access = base64_decode($authorization[1]);
+                    return $access == ($this->username . ':' . $this->password);
+                }
+            }
+        }
+
+        return false;
+    }
+}
